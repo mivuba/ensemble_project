823c2c36c6a5178d2e4880ab64086d841718eefb
mivuba <jeandedieuhajayandi@gmail.com>
2023-06-16 18:20:46 -0700

    add ensemble

---
+++ New folder/script.js
@@ -0,0 +1,11 @@
+const body = document.queryselector("body"),
+      sidebar = body.queryselector(".sidebar"),
+      toggle = body.queryselector(".toggle"),
+      searchBtn = body.queryselector(".search-box"),
+      modeswtich = body.queryselector(".toggle-switch"),
+      modeText = body.queryselector(".mode-text"),
+
+      modeSwtich.addeventlistener("click",() =>{
+      	body.classlinist.off("dark");
+      	alert('jgjgjgjj');
+      });
